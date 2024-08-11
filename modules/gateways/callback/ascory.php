9185e7be240af4b6cdef12940db4416cd0eced1d
whmcs-ru-lang <stimakkovoa@gmail.com>
2024-08-12 00:07:13 +0200

    Update ascory.php

--- modules/gateways/callback/ascory.php
+++ modules/gateways/callback/ascory.php
@@ -3,39 +3,38 @@ require_once __DIR__ . '/../../../init.php';
 require_once __DIR__ . '/../../../includes/gatewayfunctions.php';
 require_once __DIR__ . '/../../../includes/invoicefunctions.php';
 
-use WHMCS\Billing\Invoice;
 use WHMCS\Database\Capsule;
 
 $allowedIPs = ['193.222.99.133'];
 $ip = isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? explode(',', $_SERVER['HTTP_X_FORWARDED_FOR'])[0] : $_SERVER['REMOTE_ADDR'];
 if (!in_array($ip, $allowedIPs)) {
-    die('IP адрес не в вайтлисте, доступ запрещен!');
+    die('IP адрес не в белом списке, доступ запрещен!');
 }
 
 $gatewayModuleName = basename(__FILE__, '.php');
 $gatewayParams = getGatewayVariables($gatewayModuleName);
 
 $data = json_decode(file_get_contents('php://input'), true);
-$data2 = $data['data'];
-if (empty($data2['amount']) || empty($data2['id']) || empty($data2['comment']) || empty($data2['type'])) {
+if (empty($data['data']) || empty($data['data']['amount']) || empty($data['data']['id']) || empty($data['data']['comment']) || empty($data['data']['type'])) {
     logActivity("Недостаточно данных для обработки платежа");
     die('Недостаточно данных для обработки платежа');
 }
 
-$amount = $data['amount'];
-$shop = $gatewayParams['shop'];
+$data2 = $data['data'];
+$amount = $data2['amount'];
 $id = $data2['comment'];
 $status = $data2['type'];
 
 if ($status !== 'success') {
     logActivity("Ошибка при подтверждении платежа для заказа ID: $id");
-    logTransaction($gatewayParams['name'], 'Операция провалена. ID счета ' . $id . '. Сумма платежа:' . $amount . '. Статус платежа: ' . $status, 'Failure');
+    logTransaction($gatewayParams['name'], "Операция провалена. ID счета: $id. Сумма платежа: $amount. Статус платежа: $status", 'Failure');
     die('Операция провалена');
 }
+
 $invoice = Capsule::table('tblinvoices')->where('id', $id)->first();
 if (!$invoice) {
     logActivity("Ошибка при подтверждении платежа для заказа ID: $id");
-    logTransaction($gatewayParams['name'], 'Счет не найден в системе. ID счета от Ascory: ' . $id . '. Сумма платежа: ' . $amount . '. Статус платежа: ' . $status, 'Failure');
+    logTransaction($gatewayParams['name'], "Счет не найден в системе. ID счета от Ascory: $id. Сумма платежа: $amount. Статус платежа: $status", 'Failure');
     die('Счет не найден в системе');
 }
 
@@ -47,7 +46,7 @@ if ($invoice->status != 'Paid') {
         'userid' => $userId,
         'invoiceid' => $invoiceId,
         'transid' => $id,
-        'date' => date('d/m/Y'),
+        'date' => date('Y-m-d H:i:s'),
         'currencyid' => 1,
         'description' => $transactionDescription,
         'amountin' => $amount,
@@ -64,12 +63,11 @@ if ($invoice->status != 'Paid') {
             'status' => 'Paid',
             'datepaid' => date('Y-m-d H:i:s'),
         ]);
-    //addTransaction($invoiceId, 0, $amount, 0, 0, $gatewayModuleName);
-    logTransaction($gatewayParams['name'], 'Успешная оплата через' . $gatewayParams['name'] . '. ID счета: ' . $id . '. Сумма платежа: ' . $amount . '. Статус платежа: ' . $status, 'Success');
+    logTransaction($gatewayParams['name'], "Успешная оплата через {$gatewayParams['name']}. ID счета: $id. Сумма платежа: $amount. Статус платежа: $status", 'Success');
     header('HTTP/1.1 200 OK');
     echo json_encode(['code' => 200]);
 } else {
-    logTransaction($gatewayParams['name'], 'Счет уже оплачен. ID счета: ' . $id . '. Сумма платежа: ' . $amount . '. Статус платежа: ' . $status, 'Failure');
+    logTransaction($gatewayParams['name'], "Счет уже оплачен. ID счета: $id. Сумма платежа: $amount. Статус платежа: $status", 'Failure');
     die('Счет уже оплачен');
 }
 ?>
